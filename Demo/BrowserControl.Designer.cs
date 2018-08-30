76dafebaf97446f530e375d2b95fb29178d0e56b
Karen Grigoryan <wspirit97@inbox.lv>
2018-08-31 02:34:42 +0300

    da

---
+++ Demo/BrowserControl.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Demo
+{
+    partial class BrowserControl
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open browser";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OpenBrowser);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(55, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Test";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Test);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(55, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(55, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(183, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Quit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.CloseBrowser);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 194);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 71);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(153, 50);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Execute JS";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ExecuteJs);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(153, 91);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "SwitchTab";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.SwitchTab);
+            // 
+            // BrowserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "BrowserControl";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Demo/BrowserControl.cs
@@ -0,0 +1,92 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class BrowserControl : Form
+    {
+        IWebDriver Browser;
+        public BrowserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBrowser(object sender, EventArgs e)
+        {
+            Browser = new ChromeDriver();
+            Browser.Navigate().GoToUrl("http://google.com");//Go to google;
+            Browser.FindElement(By.Id("lst-ib")).SendKeys("Lego" + OpenQA.Selenium.Keys.Enter);//Find needed element and enter text, then click enter
+        }
+
+        private void CloseBrowser(object sender, EventArgs e)
+        {
+            Browser.Quit();//Closes browser
+        }
+
+        private void Test(object sender, EventArgs e)
+        {
+            Browser = new ChromeDriver();
+            Browser.Navigate().GoToUrl("http://yandex.ru");
+            Browser.FindElement(By.ClassName("desk-notif-card__login-enter-expanded")).Click();
+            Browser.FindElement(By.Name("login")).SendKeys("Zodiacasti");
+            Browser.FindElement(By.Name("passwd")).SendKeys("princpersii");
+            Browser.FindElement(By.ClassName("passport-Button")).Click();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Browser = new ChromeDriver();
+            int i = 1;
+            Browser.Navigate().GoToUrl("http://yandex.ru");
+            List<IWebElement> News = Browser.FindElements(By.CssSelector("#tabnews_newsc a")).Where(x => x.Text != "").ToList();
+            foreach(var item in News)
+            {
+                textBox1.AppendText(i + ") " + item.Text + "\n");
+                i++;
+            }
+        }
+
+        private void ExecuteJs(object sender, EventArgs e)
+        {
+            IJavaScriptExecutor jse = Browser as IJavaScriptExecutor;
+            jse.ExecuteScript("alert('ti che?')");
+        }
+
+        private void SwitchTab(object sender, EventArgs e)
+        {
+            string tab = FindWindow("habr");
+            Browser.SwitchTo().Window(tab);
+            System.Windows.Forms.MessageBox.Show(Browser.Title + "\r\n" + Browser.Url);
+        }
+
+        private string FindWindow(string value)
+        {
+            string currentWindow = Browser.CurrentWindowHandle;
+            string result = "";
+
+            for (int i = 0; i < Browser.WindowHandles.Count; i++)
+            {
+                if (Browser.WindowHandles[i] != currentWindow)
+                {
+                    Browser.SwitchTo().Window(Browser.WindowHandles[i]);
+                    if (Browser.Url.Contains(value))
+                    {
+                        result = Browser.WindowHandles[i];
+                        break;
+                    }
+                }
+            }
+            Browser.SwitchTo().Window(currentWindow);
+            return result;
+        }
+    }
+}
